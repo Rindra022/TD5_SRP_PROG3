5a2dd102b83001a6ead2f7e784c999ed631d22af
Rindra <hei.rindra.2@gmail.com>
2026-03-29 22:10:37 +0300

    Initial commit

---
+++ src/main/java/hei/td5_srp_prog3/Td5SrpProg3Application.java
@@ -0,0 +1,13 @@
+package hei.td5_srp_prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td5SrpProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td5SrpProg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hei/td5_srp_prog3/Td5SrpProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.td5_srp_prog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5SrpProg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
